92098afb2dfec13c39c258a136265c9c928711cc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-15 23:53:17 +0200

    Add arctan power series

--- PowerSeries/PowerSeries/Program.cs
+++ PowerSeries/PowerSeries/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PowerSeries
 {
@@ -7,11 +9,18 @@ namespace PowerSeries
         static void Main(string[] args)
         {
             Console.WriteLine(GetArctanUsingMathLibrary(1));
+
+            Console.WriteLine(GetArctanPowerSeries(1, 0, 10000).Sum());
         }
 
-        public static double GetArctanUsingMathLibrary(int x)
+        public static double GetArctanUsingMathLibrary(double x)
         {
             return Math.Atan(x);
+        }
+
+        public static IEnumerable<double> GetArctanPowerSeries(double x, int first, int last)
+        {
+            return Enumerable.Range(first, last).Select(r => Math.Pow(-1, r) * Math.Pow(x, 2 * r + 1) / (2 * r + 1));
         }
     }
 }
