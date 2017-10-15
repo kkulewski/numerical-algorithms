a68a8da8f44a2216b30b50e55cb11b092a7b602e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-15 23:44:47 +0200

    Add PowerSeries assignment

---
+++ PowerSeries/PowerSeries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PowerSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Atan(1));
+        }
+    }
+}
