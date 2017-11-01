d8bc770da502a5eebd5ea1751ad37586c5ce44d1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-01 21:28:48 +0100

    Add more matrix multiplication tests

--- Matrix/Test/MyMatrixTests.cs
+++ Matrix/Test/MyMatrixTests.cs
@@ -54,5 +54,55 @@ namespace Test
             Assert.Equal(10, c[1, 0]);
             Assert.Equal(17, c[1, 1]);
         }
+
+        [Fact]
+        public void NonSquare1X3M3X1MatricesMultiplicatedCorrectly()
+        {
+            var a = new MyMatrix<int>(new[,]
+            {
+                {1, 2, 3 },
+            });
+
+            var b = new MyMatrix<int>(new[,]
+            {
+                {2 },
+                {1 },
+                {2 }
+            });
+
+            var c = a * b;
+
+            // [1 2 3] * [2] = [10]
+            //         * [1] = 
+            //         * [2] =
+            Assert.Equal(10, c[0, 0]);
+        }
+
+        [Fact]
+        public void NonSquare2X3M3X2MatricesMultiplicatedCorrectly()
+        {
+            var a = new MyMatrix<int>(new[,]
+            {
+                {1, 2, 3 },
+                {4, 5, 6 }
+            });
+
+            var b = new MyMatrix<int>(new[,]
+            {
+                {1, 2 },
+                {3, 4 },
+                {5, 6 }
+            });
+
+            var c = a * b;
+
+            // [1 2 3] * [1 2] = [22 28]
+            // [4 5 6] * [3 4] = [49 64]
+            //         * [5 6] =
+            Assert.Equal(22, c[0, 0]);
+            Assert.Equal(28, c[0, 1]);
+            Assert.Equal(49, c[1, 0]);
+            Assert.Equal(64, c[1, 1]);
+        }
     }
 }
