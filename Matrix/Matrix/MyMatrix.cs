1937489e4bf23e1974216c0cc1654a3c6933dce8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-01 18:59:46 +0100

    Add MyMatrix class

---
+++ Matrix/Matrix/MyMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class MyMatrix<T>
+    {
+        private T[,] _matrix;
+
+        public MyMatrix(T[,] matrix)
+        {
+            _matrix = matrix;
+        }
+
+        public static T operator +(MyMatrix<T> a, MyMatrix<T> b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T operator *(MyMatrix<T> a, MyMatrix<T> b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
