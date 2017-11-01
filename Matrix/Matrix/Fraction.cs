2b9d17ac6a93da503de1e6974463052a6556b8b0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-01 22:30:23 +0100

    Add fraction struct

---
+++ Matrix/Matrix/Fraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Matrix
+{
+    public struct Fraction
+    {
+        private BigInteger _numerator;
+        private BigInteger _denominator;
+
+        public Fraction(BigInteger numerator, BigInteger denominator)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+    }
+}
