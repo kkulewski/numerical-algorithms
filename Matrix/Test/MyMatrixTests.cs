752163aa2bc98c33a610b026e8efca5a2cd856b3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-01 20:03:23 +0100

    Add square matrices addition test

--- Matrix/Test/MyMatrixTests.cs
+++ Matrix/Test/MyMatrixTests.cs
@@ -5,5 +5,30 @@ namespace Test
 {
     public class MyMatrixTests
     {
+        [Fact]
+        public void SquareMatricesAreAddedCorrectly()
+        {
+            var a = new MyMatrix<int>(new[,]
+            {
+                {1, 2},
+                {3, 4}
+            });
+
+            var b = new MyMatrix<int>(new[,]
+            {
+                {2, 3},
+                {1, 2}
+            });
+
+            var c = a + b;
+
+            // expected:
+            // [3 5]
+            // [4 6]
+            Assert.Equal(3, c[0, 0]);
+            Assert.Equal(5, c[0, 1]);
+            Assert.Equal(4, c[1, 0]);
+            Assert.Equal(6, c[1, 1]);
+        }
     }
 }
