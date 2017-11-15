177687e0205bcce600c3c7b1b120c423feacb3c6
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-16 00:35:52 +0100

    Add tester class

---
+++ Matrix/Matrix/Tester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Tester
+    {
+        private readonly Random _random = new Random();
+
+        public void WriteFractionMatrix(int matrixSize, string fileName)
+        {
+            var m = new MyMatrix<Fraction>(matrixSize, matrixSize);
+            File.WriteAllText(fileName, MyMatrixWriter.GetFormattedMatrix(m));
+        }
+
+        public void WriteFractionVector(int matrixSize, string fileName)
+        {
+            var v = new Fraction[matrixSize];
+            for (var i = 0; i < matrixSize; i++)
+                v[i] = new Fraction(_random.Next(), _random.Next(1, int.MaxValue));
+
+            File.WriteAllText(fileName, MyMatrixWriter.GetFormattedVector(v));
+        }
+    }
+}
