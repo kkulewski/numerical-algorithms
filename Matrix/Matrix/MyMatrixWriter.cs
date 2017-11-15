f1644a39eac8af6cc75d2226840dc5c06c8e0cc8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-16 00:10:03 +0100

    Add matrix writer

---
+++ Matrix/Matrix/MyMatrixWriter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Matrix
+{
+    public static class MyMatrixWriter
+    {
+        public static string GetFormattedMatrix<T>(MyMatrix<T> matrix) where T : new()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < matrix.Rows; i++)
+            {
+                sb.Append("[");
+                for (var j = 0; j < matrix.Cols; j++)
+                {
+                    sb.Append(matrix[i, j]);
+                    if(j < matrix.Cols-1)
+                        sb.Append(";");
+                }
+
+                sb.Append("]");
+                if (i < matrix.Rows - 1)
+                    sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public static string GetFormattedVector<T>(T[] vector) where T : new()
+        {
+            var sb = new StringBuilder();
+            sb.Append("[");
+            for (var i = 0; i < vector.Length; i++)
+            {
+                sb.Append(vector[i]);
+                if (i < vector.Length - 1)
+                    sb.Append(";");
+            }
+
+            sb.Append("]");
+            return sb.ToString();
+        }
+    }
+}
