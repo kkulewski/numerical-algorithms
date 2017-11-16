062adbd7ff5e662aa857d976a2f173578f42dfc1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-16 20:33:02 +0100

    Add fraction simplification on init test

--- Matrix/Test/FractionTests.cs
+++ Matrix/Test/FractionTests.cs
@@ -7,9 +7,26 @@ namespace Test
     public class FractionTests
     {
         [Fact]
-        public static void ThrowsOn_FractionInit_When_Denominator_Equals_0()
+        public void ThrowsOn_FractionInit_When_Denominator_Equals_0()
         {
             Assert.Throws<ArgumentException>(() => new Fraction(1, 0));
+        }
+
+        [Fact]
+        public void Simplifies_Fractions_On_Init()
+        {
+            var f1 = new Fraction(2, 4);
+            var f2 = new Fraction(6, 9);
+            var f3 = new Fraction(12, 2);
+
+            Assert.Equal(1, f1.Numerator);
+            Assert.Equal(2, f1.Denominator);
+
+            Assert.Equal(2, f2.Numerator);
+            Assert.Equal(3, f2.Denominator);
+
+            Assert.Equal(6, f3.Numerator);
+            Assert.Equal(1, f3.Denominator);
         }
     }
 }
