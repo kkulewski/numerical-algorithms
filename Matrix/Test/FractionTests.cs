f29474fe8604cc2ee62566a4d1101d92763162a3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-16 20:27:33 +0100

    Add fraction tests

---
+++ Matrix/Test/FractionTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Matrix;
+using Xunit;
+
+namespace Test
+{
+    public class FractionTests
+    {
+        [Fact]
+        public static void ThrowsOn_FractionInit_When_Denominator_Equals_0()
+        {
+            Assert.Throws<ArgumentException>(() => new Fraction(1, 0));
+        }
+    }
+}
