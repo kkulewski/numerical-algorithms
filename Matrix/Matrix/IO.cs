af4dc7ae4cc6794042eeba25064641044a14ad58
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-17 23:56:18 +0100

    Refactor IO consts

--- Matrix/Matrix/IO.cs
+++ Matrix/Matrix/IO.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Matrix
+﻿namespace Matrix
 {
     public class IO
     {
+        public const string FileType = ".txt";
+
         public const string PrefixFraction = "fraction_";
         public const string PrefixDouble = "double_";
         public const string PrefixFloat = "float_";
 
-        public const string FileA = "a.txt";
-        public const string FileB = "b.txt";
-        public const string FileC = "c.txt";
-        public const string FileX = "x.txt";
+        public const string FileA = "a" + FileType;
+        public const string FileB = "b" + FileType;
+        public const string FileC = "c" + FileType;
+        public const string FileX = "x" + FileType;
 
-        public const string ResultAx = "result_ax.txt";
-        public const string ResultAbcx = "result_abcx.txt";
-        public const string ResultAbc = "result_abc.txt";
+        public const string PrefixResult = "result_";
+        public const string PrefixEigen = "eigen_";
 
-        public const string ResultNoPivot = "result_no_pivot.txt";
-        public const string ResultPartialPivot = "result_partial_pivot.txt";
-        public const string ResultFullPivot = "result_full_pivot.txt";
+        public const string ResultAx = PrefixResult + "ax" + FileType;
+        public const string ResultAbcx = PrefixResult + "abcx" + FileType;
+        public const string ResultAbc = PrefixResult + "abc" + FileType;
+
+        public const string ResultNoPivot = PrefixResult + "no" + FileType;
+        public const string ResultPartialPivot = PrefixResult + "partial" + FileType;
+        public const string ResultFullPivot = PrefixResult + "full" + FileType;
+
     }
 }
