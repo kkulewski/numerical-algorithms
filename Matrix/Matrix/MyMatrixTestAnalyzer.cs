33cc3c0a4422cbd1e7be8e22f906ca9f794d8ee9
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-18 00:41:46 +0100

    Add matrix and vector norm impl

--- Matrix/Matrix/MyMatrixTestAnalyzer.cs
+++ Matrix/Matrix/MyMatrixTestAnalyzer.cs
@@ -18,17 +18,39 @@ namespace Matrix
 
         public void CompareGaussPartialPivot()
         {
-            var handler = new MyMatrixIoHandler();
 
-            var fractionTime = handler.LoadDoubleVector(IO.PrefixFraction + IO.ResultPartialPivot, true).Item2;
-            var doubleTime = handler.LoadDoubleVector(IO.PrefixDouble + IO.ResultPartialPivot, true).Item2;
-            var floatTime = handler.LoadFloatVector(IO.PrefixFloat + IO.ResultPartialPivot, true).Item2;
-            var doubleTimeEigen = handler.LoadDoubleVector(IO.PrefixEigen + IO.PrefixDouble + IO.ResultPartialPivot, true).Item2;
-            var floatTimeEigen = handler.LoadFloatVector(IO.PrefixEigen + IO.PrefixFloat + IO.ResultPartialPivot, true).Item2;
+            var fr = _handler.LoadDoubleVector(IO.PrefixFraction + IO.ResultPartialPivot, true);
 
-             Console.WriteLine("fr: {0}\ndo: {1}\nfl: {2}\ned: {3}\nef: {4}", 
-                fractionTime, doubleTime, floatTime, 
-               doubleTimeEigen, floatTimeEigen);
+            var d = _handler.LoadDoubleVector(IO.PrefixDouble + IO.ResultPartialPivot, true);
+            var f = _handler.LoadFloatVector(IO.PrefixFloat + IO.ResultPartialPivot, true);
+
+            var ed = _handler.LoadDoubleVector(IO.PrefixEigen + IO.PrefixDouble + IO.ResultPartialPivot, true);
+            var ef = _handler.LoadFloatVector(IO.PrefixEigen + IO.PrefixFloat + IO.ResultPartialPivot, true);
+        }
+
+        private static double VectorNorm<T1, T2>(T1[] vector, T2[] refVector)
+        {
+            var sum = 0.0;
+            for (var i = 0; i < vector.Length; i++)
+            {
+                sum += (refVector[i] - (dynamic) vector[i]) * (refVector[i] - (dynamic) vector[i]);
+            }
+
+            return Math.Sqrt(sum);
+        }
+
+        private static double MatrixNorm<T1, T2>(MyMatrix<T1> matrix, MyMatrix<T2> refMatrix) where T1: new() where T2 : new()
+        {
+            var sum = 0.0;
+            for (var i = 0; i < matrix.Rows; i++)
+            {
+                for (var j = 0; j < matrix.Cols; j++)
+                {
+                    sum += (refMatrix[i, j] - (dynamic) matrix[i, j]) * (refMatrix[i, j] - (dynamic) matrix[i, j]);
+                }
+            }
+
+            return Math.Sqrt(sum);
         }
     }
 }
