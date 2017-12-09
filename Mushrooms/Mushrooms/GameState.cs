eae1a86ffcec103132b683e1d1717a1283202018
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-09 01:25:03 +0100

    Add game states

---
+++ Mushrooms/Mushrooms/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms
+{
+    public class GameState
+    {
+        public int GameStateId;
+
+        public int Player1Position;
+
+        public int Player2Position;
+
+        public bool IsPlayer1Turn;
+
+        public List<int> PossibleGameStates = new List<int>();
+
+        public GameState(int gameStateId, int player1Position, int player2Position, bool isPlayer1Turn)
+        {
+            GameStateId = gameStateId;
+            Player1Position = player1Position;
+            Player2Position = player2Position;
+            IsPlayer1Turn = isPlayer1Turn;
+        }
+    }
+}
--- Mushrooms/Mushrooms/Program.cs
+++ Mushrooms/Mushrooms/Program.cs
@@ -10,6 +10,29 @@ namespace Mushrooms
     {
         static void Main(string[] args)
         {
+            // k = 3 + 4%6 == 7
+            var n = 2;
+            var indices = 2;
+            var indicesStart = -1;
+            var indicesEnd = 1;
+
+            var boardSize = 2 * n + 1;
+
+            var gameStates = new Dictionary<int, GameState>();
+            var gameStateId = 0;
+            for (var turn = 0; turn < 2; turn++)
+            {
+                for (var player1Pos = -n; player1Pos <= n; player1Pos++)
+                {
+                    for (var player2Pos = -n; player2Pos <= n; player2Pos++)
+                    {
+                        gameStateId++;
+                        var isPlayer1Turn = turn == 0;
+                        var gameState = new GameState(gameStateId, player1Pos, player2Pos, isPlayer1Turn);
+                        gameStates.Add(gameStateId, gameState);
+                    }
+                }
+            }
         }
     }
 }
