3316dfb2d732b98f937e514d42d328eddde9295d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-09 02:55:45 +0100

    Fix transitions

--- Mushrooms/Mushrooms/Program.cs
+++ Mushrooms/Mushrooms/Program.cs
@@ -11,20 +11,22 @@ namespace Mushrooms
         static void Main(string[] args)
         {
             // k = 3 + 4%6 == 7
-            var n = 2;
+            var n = 1;
             var indices = 2;
             var indicesStart = -1;
             var indicesEnd = 1;
 
             var boardSize = 2 * n + 1;
 
+            // GENERATE ALL POSSIBLE GAME STATES
             var gameStates = new Dictionary<int, GameState>();
             var gameStateId = 0;
-            for (var turn = 0; turn < 2; turn++)
+
+            for (var player1Pos = -n; player1Pos <= n; player1Pos++)
             {
-                for (var player1Pos = -n; player1Pos <= n; player1Pos++)
+                for (var player2Pos = -n; player2Pos <= n; player2Pos++)
                 {
-                    for (var player2Pos = -n; player2Pos <= n; player2Pos++)
+                    for (var turn = 0; turn < 2; turn++)
                     {
                         var isPlayer1Turn = turn == 0;
                         var gameState = new GameState(gameStateId, player1Pos, player2Pos, isPlayer1Turn);
@@ -34,23 +36,25 @@ namespace Mushrooms
                 }
             }
 
+            // ASSIGN TRANSITION LIST TO EACH STATE
             foreach (var currentState in gameStates.Values)
             {
                 var transitions = new List<int>();
                 foreach (var nextState in gameStates.Values)
                 {
-                    var player1Waited = nextState.Player1Position == currentState.Player1Position;
-                    var player1Moved = nextState.Player1Position == currentState.Player1Position + 1 ||
-                                        nextState.Player1Position == currentState.Player1Position + -1;
+                    var player1Waits = nextState.Player1Position == currentState.Player1Position;
+                    var player1Moves = nextState.Player1Position == (currentState.Player1Position + 1) ||
+                                        nextState.Player1Position == (currentState.Player1Position - 1);
 
-                    var player2Waited = nextState.Player2Position == currentState.Player2Position;
-                    var player2Moved = nextState.Player2Position == currentState.Player2Position + 1 ||
-                                        nextState.Player2Position == currentState.Player2Position + -1;
+                    var player2Waits = nextState.Player2Position == currentState.Player2Position;
+                    var player2Moves = nextState.Player2Position == (currentState.Player2Position + 1) ||
+                                        nextState.Player2Position == (currentState.Player2Position - 1);
 
-                    var turnChanged = nextState.IsPlayer1Turn != currentState.IsPlayer1Turn;
+                    var turnChanges = nextState.IsPlayer1Turn != currentState.IsPlayer1Turn;
+                    var isPlayer1Turn = currentState.IsPlayer1Turn;
 
-                    var possibleTransition = turnChanged && player1Moved && player2Waited ||
-                                              turnChanged && player1Waited && player2Moved;
+                    var possibleTransition = (isPlayer1Turn && player1Moves && player2Waits && turnChanges) ||
+                                              (!isPlayer1Turn && player1Waits && player2Moves && turnChanges);
 
                     if (possibleTransition)
                         transitions.Add(nextState.GameStateId);
@@ -59,26 +63,24 @@ namespace Mushrooms
                 currentState.Transitions = transitions;
             }
 
+            // FILL MATRIX WITH TRANSITIONS
             var size = gameStates.Count;
             var stateMatrix = new double[size, size];
             for (var row = 0; row < size; row++)
             {
                 var state = gameStates[row];
-                for (var col = 0; col < size; col++)
+                foreach (var transition in state.Transitions)
                 {
-                    foreach (var transition in state.Transitions)
-                    {
-                        stateMatrix[row, transition] = -1.0 / indices;
-                    }
-
+                    stateMatrix[row, transition] = 1.0 / indices;
                 }
 
                 stateMatrix[row, row] = 1;
             }
 
+            // PRINT MATRIX
             for (var i = 0; i < size; i++)
             {
-                Console.Write("[ ");
+                Console.Write("{0:D2} [ ", i);
                 for (var j = 0; j < size; j++)
                 {
                     Console.Write("{0:N1} ", stateMatrix[i, j]);
