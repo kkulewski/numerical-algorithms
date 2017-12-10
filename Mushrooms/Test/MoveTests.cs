52d3bc730329066855647c56d3047f58f2d1d3b0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-10 03:10:47 +0100

    Add move without crossing 0 or N tests

---
+++ Mushrooms/Test/MoveTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mushrooms;
+using Xunit;
+
+namespace Test
+{
+    public class MoveTests
+    {
+        [Fact]
+        public void ValidMoves_When_N4_D2_No0Cross_NoNCross()
+        {
+            var n = 4;
+            var boardSize = 2 * n + 1;
+            int startPosition = 2;
+            var endPositionForGivenToss = new Dictionary<int, int>
+            {
+                [-2] = 0,
+                [-1] = 1,
+                [1] = 3,
+                [2] = 4
+            };
+
+            var validMoves =
+                endPositionForGivenToss
+                .All(x => Program.CheckIfValidMove(startPosition, x.Value, x.Key, boardSize));
+            Assert.True(validMoves);
+
+            var invalidMovesPlus =
+                endPositionForGivenToss
+                .Any(x => Program.CheckIfValidMove(startPosition, x.Value + 1, x.Key, boardSize));
+            Assert.False(invalidMovesPlus);
+
+            var invalidMovesMinus =
+                endPositionForGivenToss
+                .Any(x => Program.CheckIfValidMove(startPosition, x.Value - 1, x.Key, boardSize));
+            Assert.False(invalidMovesMinus);
+        }
+    }
+}
