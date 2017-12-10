56dac801e0e33af602b6bdc98263401364bba3a8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-11 00:52:24 +0100

    Add dice class

---
+++ Mushrooms/Mushrooms/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Mushrooms
+{
+    public class Dice : IEnumerable
+    {
+        public IDictionary<int, double> Indices;
+
+        public double this[int index]
+        {
+            get => Indices[index];
+            set => Indices[index] = value;
+        }
+
+        public Dice(IDictionary<int, double> indices)
+        {
+            Indices = indices;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Indices.GetEnumerator();
+        }
+    }
+}
