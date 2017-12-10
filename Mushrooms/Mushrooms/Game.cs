ac7b10a1e9f84d6f3ec94f2cf813b1b690d2fadd
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-10 23:07:22 +0100

    Add game class

---
+++ Mushrooms/Mushrooms/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms
+{
+    class Game
+    {
+        public int BoardSize;
+
+        public List<Tuple<int, double>> Dice;
+
+        public int Player1InitialPosition;
+
+        public int Player2InitialPosition;
+    }
+}
