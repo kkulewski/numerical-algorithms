bac857df1a7391551853cf319521dfb6d0c16d77
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-10 03:12:26 +0100

    Add move test with N crossing

--- Mushrooms/Test/MoveTests.cs
+++ Mushrooms/Test/MoveTests.cs
@@ -39,5 +39,35 @@ namespace Test
                 .Any(x => Program.CheckIfValidMove(startPosition, x.Value - 1, x.Key, boardSize));
             Assert.False(invalidMovesMinus);
         }
+
+        [Fact]
+        public void ValidMoves_When_N4_D2_No0Cross_NCross()
+        {
+            var n = 4;
+            var boardSize = 2 * n + 1;
+            int startPosition = 4;
+            var endPositionForGivenToss = new Dictionary<int, int>
+            {
+                [-2] = 2,
+                [-1] = 3,
+                [1] = -4,
+                [2] = -3
+            };
+
+            var validMoves =
+                endPositionForGivenToss
+                    .All(x => Program.CheckIfValidMove(startPosition, x.Value, x.Key, boardSize));
+            Assert.True(validMoves);
+
+            var invalidMovesPlusOne =
+                endPositionForGivenToss
+                    .Any(x => Program.CheckIfValidMove(startPosition, x.Value + 1, x.Key, boardSize));
+            Assert.False(invalidMovesPlusOne);
+
+            var invalidMovesMinusOne =
+                endPositionForGivenToss
+                    .Any(x => Program.CheckIfValidMove(startPosition, x.Value - 1, x.Key, boardSize));
+            Assert.False(invalidMovesMinusOne);
+        }
     }
 }
