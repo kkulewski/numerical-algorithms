9c3718febd703f1b014f79fdfcfeeb07a60f05e2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-11 02:15:49 +0100

    Run simple game + compute win chance in main

--- Mushrooms/Mushrooms/Program.cs
+++ Mushrooms/Mushrooms/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Mushrooms
 {
@@ -10,149 +6,37 @@ namespace Mushrooms
     {
         static void Main(string[] args)
         {
-            // k = 3 + 4%6 == 7
             var n = 4;
             var boardSize = 2 * n + 1;
 
-            var indiceList = new List<Tuple<int, double>>
-            {
-                new Tuple<int, double>(-2, 0.25),
-                new Tuple<int, double>(-1, 0.25),
-                new Tuple<int, double>(1, 0.25),
-                new Tuple<int, double>(2, 0.25)
-            };
+            var p1Pos = n;
+            var p2Pos = -n;
 
-
-            // GENERATE ALL POSSIBLE GAME STATES
-            var gameStates = new Dictionary<int, GameState>();
-            var gameStateId = 0;
-
-            for (var player1Pos = -n; player1Pos <= n; player1Pos++)
-            {
-                for (var player2Pos = -n; player2Pos <= n; player2Pos++)
+            var dice = new Dice
+            (
+                new Dictionary<int, double>
                 {
-                    if (player1Pos == 0 && player2Pos == 0)
-                        continue;
-
-                    for (var turn = 0; turn < 2; turn++)
-                    {
-                        var isPlayer1Turn = turn == 0;
-                        var gameState = new GameState(gameStateId, player1Pos, player2Pos, isPlayer1Turn);
-                        gameStates[gameStateId] = gameState;
-                        gameStateId++;
-                    }
+                    [-2] = 0.25,
+                    [-1] = 0.25,
+                    [1] = 0.25,
+                    [2] = 0.25
                 }
-            }
+            );
+
+            var game = new Game(boardSize, p1Pos, p2Pos, dice);
+            game.GeneratePossibleStates();
+            game.GeneratePossibleTransitions();
 
 
-            // ASSIGN TRANSITION LIST TO EACH STATE
-            foreach (var currentState in gameStates.Values)
-            {
-                currentState.Transitions = new List<Tuple<int, double>>();
+            // SOLVE
+            var mv = game.GetGameMatrixAndProbabilityVector();
+            var stateMatrix = new MyMatrix<double>(mv.Item1);
+            var probabilityVector = mv.Item2;
+            var iterations = 100;
 
-                bool gameFinished = currentState.Player1Position == 0 ||
-                                    currentState.Player2Position == 0;
-                if(gameFinished)
-                    continue;
-
-                foreach (var nextState in gameStates.Values)
-                {
-                    bool turnChanges = currentState.IsPlayer1Turn != nextState.IsPlayer1Turn;
-
-                    foreach (var toss in indiceList)
-                    {
-                        bool isPlayer1Turn = currentState.IsPlayer1Turn;
-                        bool player1Moves = CheckIfValidMove(currentState.Player1Position, nextState.Player1Position, toss.Item1, boardSize);
-                        bool player2Waits = nextState.Player2Position == currentState.Player2Position;
-
-                        bool isPlayer2Turn = !currentState.IsPlayer1Turn;
-                        bool player2Moves = CheckIfValidMove(currentState.Player2Position, nextState.Player2Position, toss.Item1, boardSize);
-                        bool player1Waits = nextState.Player1Position == currentState.Player1Position;
-
-                        bool possibleTransition = isPlayer1Turn && player1Moves && player2Waits && turnChanges ||
-                                                  isPlayer2Turn && player2Moves && player1Waits && turnChanges;
-
-                        bool alreadyInList = currentState.Transitions.Select(s => s.Item1).Contains(nextState.GameStateId);
-
-                        if (possibleTransition && !alreadyInList)
-                        {
-                            currentState.Transitions.Add(new Tuple<int, double>(nextState.GameStateId, toss.Item2));
-                        }
-                    }
-                }
-            }
-
-
-            // FILL MATRIX WITH TRANSITIONS
-            var size = gameStates.Count;
-            var stateMatrix = new double[size, size];
-            var probabilityVector = new double[size];
-            for (var row = 0; row < size; row++)
-            {
-                stateMatrix[row, row] = 1;
-                var state = gameStates[row];
-
-                bool player1Won = state.Player1Position == 0;
-                bool player2Won = state.Player2Position == 0;
-
-                if (player1Won)
-                {
-                    probabilityVector[row] = 1;
-                    continue;
-                }
-                
-                if (player2Won)
-                {
-                    probabilityVector[row] = 0;
-                    continue;
-                }
-
-                foreach (var transition in state.Transitions)
-                {
-                    stateMatrix[row, transition.Item1] = -transition.Item2;
-                }
-            }
-
-
-            // PRINT MATRIX
-            for (var i = 0; i < size; i++)
-            {
-                Console.Write("{0:D2} [ ", i);
-                for (var j = 0; j < size; j++)
-                {
-                    Console.Write("{0:N1} ", stateMatrix[i, j]);
-                }
-                Console.WriteLine(" ] [ {0} ]", probabilityVector[i]);
-            }
-
-
-            var mymatrix = new MyMatrix<double>(stateMatrix);
-            //mymatrix.Jacobi(probabilityVector, 100);
-            mymatrix.GaussianReductionNoPivot(probabilityVector);
-
-        }
-
-        public static bool CheckIfValidMove(int startPosition, int endPosition, int toss, int boardSize)
-        {
-            var n = (boardSize - 1) / 2;
-
-            bool forwardNoCross = (toss > 0)
-                && (startPosition + toss <= n)
-                && (endPosition == startPosition + toss);
-
-            bool backwardNoCross = (toss < 0)
-                && (startPosition - toss >= -n)
-                && (endPosition == startPosition + toss);
-
-            bool forwardCross = (toss > 0)
-                && (startPosition + toss > n)
-                && (endPosition == -n + (toss - 1 - (n - startPosition)));
-
-            bool backwardCross = (toss < 0)
-                && (startPosition + toss < -n)
-                && (endPosition == n + (toss + 1 + (n + startPosition)));
-            
-            return forwardNoCross || backwardNoCross || forwardCross || backwardCross;
+            //stateMatrix.GaussianReductionPartialPivot(probabilityVector);
+            //stateMatrix.Jacobi(probabilityVector, iterations);
+            stateMatrix.GaussSeidel(probabilityVector, iterations);
         }
     }
 }
