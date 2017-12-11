f4e37ccbb7b3f55a1564ebb37aa9746d4ed222d1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-11 03:26:29 +0100

    Add myMatrix IO handler from previous project

---
+++ Mushrooms/Mushrooms/IO/MyMatrixIOHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Mushrooms.IO
+{
+    public static class MyMatrixIoHandler
+    {
+        private static void WriteToFile(string fileName, string text, int matrixSize)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(matrixSize.ToString());
+            sb.AppendLine(text);
+
+            File.WriteAllText(fileName, sb.ToString());
+        }
+
+        public static void WriteToFileWithTimespan(string fileName, string text, int matrixSize, double time)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(time.ToString());
+            sb.AppendLine(matrixSize.ToString());
+            sb.AppendLine(text);
+
+            File.WriteAllText(fileName, sb.ToString());
+        }
+
+        public static void WriteMatrixToFile<T>(MyMatrix<T> matrix, string fileName) where T : new()
+        {
+            var formattedMatrix = MyMatrixFormatter.GetFormattedMatrix(matrix);
+            WriteToFile(fileName, formattedMatrix, matrix.Rows);
+        }
+
+        public static void WriteVectorToFile<T>(T[] vector, string fileName) where T : new()
+        {
+            var formattedVector = MyMatrixFormatter.GetFormattedVector(vector);
+            WriteToFile(fileName, formattedVector, vector.Length);
+        }
+    }
+}
