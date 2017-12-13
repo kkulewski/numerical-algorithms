109e52da39343e0f263955112a0b2ff52943ad12
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-14 00:23:49 +0100

    Add result file names

--- Mushrooms/Mushrooms/IO/IO.cs
+++ Mushrooms/Mushrooms/IO/IO.cs
@@ -5,5 +5,12 @@
         public const string FileType = ".txt";
         public const string Matrix = "m" + FileType;
         public const string Vector = "v" + FileType;
+        
+        public const string PrefixCsharp = "csh_";
+        public const string PrefixEigen = "eigen_";
+
+        public const string CsharpJacobi = PrefixCsharp + "jacobi" + FileType;
+        public const string CsharpGaussSeidel = PrefixCsharp + "gauss-seidel" + FileType;
+        public const string CsharpGaussPartialPivot = PrefixCsharp + "gauss-partial" + FileType;
     }
 }
