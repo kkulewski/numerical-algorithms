c893e365ac7da2b7fd93846080e17c1813f08e23
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-13 23:47:16 +0100

    Add file name consts

---
+++ Mushrooms/Mushrooms/IO/IO.cs
@@ -0,0 +1,9 @@
+﻿namespace Mushrooms.IO
+{
+    public class IO
+    {
+        public const string FileType = ".txt";
+        public const string Matrix = "m" + FileType;
+        public const string Vector = "v" + FileType;
+    }
+}
