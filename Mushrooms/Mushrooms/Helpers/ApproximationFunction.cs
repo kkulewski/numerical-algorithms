ae6c871b6d3b53f754d9ef5b93e074edab38911e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-11 23:19:05 +0100

    Add approximation function + approximator

---
+++ Mushrooms/Mushrooms/Helpers/ApproximationFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms.Helpers
+{
+    public class ApproximationFunction
+    {
+        private readonly double[] _polynomial;
+
+        public ApproximationFunction(double[] polynomial)
+        {
+            _polynomial = polynomial;
+        }
+
+        public double GetResult(double argument)
+        {
+            var result = 0.0;
+            for (var i = 0; i < _polynomial.Length; i++)
+            {
+                result += _polynomial[i] * Math.Pow(argument, i);
+            }
+
+            return result;
+        }
+    }
+}
